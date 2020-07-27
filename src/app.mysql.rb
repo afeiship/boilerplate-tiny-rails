50ea4f3587ea605d57bce9ae06cdb7256462ea2b
afeiship <1290657123@qq.com>
2020-07-27 19:05:13 +0800

    add: type app

---
+++ src/app.mysql.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# Instead of loading all of Rails, load the
+# particular Rails dependencies we need
+
+require_relative "./initialize"
+
+class App
+  def start
+    # SpiderPage.start
+    records = Page.where(grabbed: false)
+    size = records / 50
+    threads = records.each_slice(size).map do |items|
+      Thread.new do
+        items.each do |item|
+          puts "url: #{item.url}"
+          SpiderUrl.start(item.url)
+          item.update(grabbed: true)
+        end
+      end
+    end
+
+    threads.each(&:join)
+  end
+end
+
+app = App.new
+app.start
